96de15bc29e36f900d0ca12b3cbdf01e0e4bbb26
maybeayush <155345759+maybeayush@users.noreply.github.com>
2026-05-29 23:03:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Hey You Know What! am blessed with you💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
